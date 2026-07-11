710e1c8a839f64a914b070fca7a7e6743131ee12
obulbuk07 <obulbuk07@gamil.com>
2026-07-11 23:14:22 +0200

    feat: add edit button on skill card

--- src/components/skillCard.jsx
+++ src/components/skillCard.jsx
@@ -1,8 +1,11 @@
 import {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+
+
 import ContactModal from './ContactModal'
 function SkillCard({id, name, category, author, canTeach, onDelete, isOwner}){
     const[showModal, setShowModal] = useState(false)
-
+    const navigate = useNavigate()
     return(
         <div className="min-h-[160px] relative flex flex-col bg-white rounded-xl p-4 border border-gray-200 hover:shadow-md transition-shadow cursor-pointer">
             <span className={`self-start inline-block text-xs px-2 py-1 rounded-full ${canTeach ? 'bg-green-100 text-green-700' : 'bg-yellow-100 text-yellow-700'}`}>{canTeach? "Can Teach" : "Want to learn"}</span>
@@ -14,12 +17,16 @@ function SkillCard({id, name, category, author, canTeach, onDelete, isOwner}){
                 </div>
                 <span className="text-xs text-gray-400">{author}</span>
                 <button className='absolute bottom-5 right-5 hover:underline transition duration-' type="button" onClick={() => setShowModal(true) } >Contact</button>
-                {isOwner &&
-                    <button onClick={onDelete} className="text-xs absolute top-5 right-5 text-red-400 hover:text-red-600">
-                        Delete
-                    </button>
-            
-                }
+                {isOwner && (
+                    <>
+                        <button onClick={onDelete} className="text-xs absolute top-5 right-5 text-red-400 hover:text-red-600">
+                            Delete
+                        </button>
+                        <button onClick={() => navigate(`/edit/${id}`)} className="text-xs absolute top-5 right-16 text-blue-400 hover:text-blue-600">
+                            Edit
+                        </button>
+                    </>
+                )}
                 
             </div>
             {showModal && <ContactModal
