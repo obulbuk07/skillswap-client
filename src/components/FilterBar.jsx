1d9a69c93116c0c59eb0937a3679393545bb38e4
obulbuk07 <obulbuk07@gamil.com>
2026-07-11 22:43:57 +0200

    Add add skill button to filer bar

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,11 +1,12 @@
-
+import {Link} from 'react-router-dom'
 const categories = ['All', 'Frontend', 'Backend', 'Design', 'Data', "Other"]
 const canTeachCategories = ['All', 'Can teach', 'Want to learn']
-function FilterBar({onCategoryChange, activeCategory, onCanTeachCategoryChange, activeCanTeachCategory}){
 
+function FilterBar({onCategoryChange, activeCategory, onCanTeachCategoryChange, activeCanTeachCategory}){
+    const token = localStorage.getItem('token')
     return(
-        <div>
-            <div className="px-6 py-3">
+        <div className='flex justify-between p-4'>
+            <div className="px-6 flex gap-4">
                 {categories.map(cat => (
                     <button
                         key = {cat}
@@ -17,17 +18,19 @@ function FilterBar({onCategoryChange, activeCategory, onCanTeachCategoryChange, 
                 ))}
             
             </div>
-            <div>
+            <div className='flex gap-4'>
                 {canTeachCategories.map(canTeachcat =>( 
                     <button
                     key={canTeachcat}
                     onClick={() => onCanTeachCategoryChange(canTeachcat)}
-                    className={`px-3 py-1 rounded-full text-sm ${activeCanTeachCategory === canTeachcat ? 'bg-blue-600 text-white' : 'bg-gray-100 text-gray-600'}`}
+                    className={`px-3 py-1 rounded-full text-sm ${activeCanTeachCategory === canTeachcat ? 'bg-green-600 text-white' : 'bg-gray-100 text-gray-600'}`}
                     >
                         {canTeachcat}
                     </button>
                 ))}
             </div>
+            {token && <Link to="/add" className="bg-blue-600 text-white px-4 py-2 rounded-lg text-sm">Add Skill</Link> }
+            
         </div>
         
         
